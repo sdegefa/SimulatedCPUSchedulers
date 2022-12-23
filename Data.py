12ddffdc571659fe4b119da41f55100b23464a35
Solan Degefa <119986614+sdegefa@users.noreply.github.com>
2022-12-22 19:04:40 -0600

    Add files via upload

--- Data.py
+++ Data.py
@@ -1,39 +1,19 @@
-
-
-executionTime = 0
-processQueue2 = []
-processQueue3 = []
-
 readyQueue = []
-cpuQueue = []
-finishedProcesses = []
-waitingInIO = []
-
 #                   fcfs  sjf  mlfq
-cpuUtilization =      [0, 0, 0]
+cpuUtilization = [0, 0, 0]
 finalExecutionTimes = [0, 0, 0]
 
-#             Tr   Tw   Ttr
-#             [0]  [0]  [0]
+#                Tr  Tw  Ttr
+#                [0] [0] [0]
 avgTimes = [
     [0, 0, 0],  # fcfs
     [0, 0, 0],  # sjf
     [0, 0, 0],  # mlfq
 ]
 
-#   Has Run  pID     Tr     Tw     Ttr
-#     [0]     [0]    [0]    [0]     [0]
-dataBank = [
-    [0, 0, 0, 0, 0],  # p1
-    [0, 0, 0, 0, 0],  # p2
-    [0, 0, 0, 0, 0],  # p3
-    [0, 0, 0, 0, 0],  # p4
-    [0, 0, 0, 0, 0],  # p5
-    [0, 0, 0, 0, 0],  # p6
-    [0, 0, 0, 0, 0],  # p7
-    [0, 0, 0, 0, 0]  # p8
-]
-
+# All Databanks have the same formating
+#  Has-Run pID  Tr   Tw   Ttr
+#     [0]  [0]  [0]  [0]  [0]
 dataBankFCFS = [
     [0, 0, 0, 0, 0],  # p1
     [0, 0, 0, 0, 0],  # p2
@@ -69,11 +49,12 @@ dataBankMLFQ = [
 
 dataBanks = [dataBankFCFS, dataBankSJF, dataBankMLFQ]
 
+
 def add(processs):
     readyQueue.append(processs)
 
 
-def decIO(waitingInIO, processList,readyQueue):
+def decIO(waitingInIO, processList, readyQueue):
     try:
         # for process in range(len(waitingInIO)):
         processs = 0
@@ -101,29 +82,6 @@ def decIO(waitingInIO, processList,readyQueue):
         exit()
 
 
-def saveData(dataBankIndex, wastedTime=None):
-    avgTat = 0
-    avgTr = 0
-    avgTwt = 0
-    cpuUtilTemp = 0
-    finishedProcesses.sort()
-    finalExecutionTimes[dataBankIndex] = executionTime
-    cpuUtilTemp = ((executionTime - wastedTime) / executionTime) * 100
-    cpuUtilization[dataBankIndex] = round(cpuUtilTemp, 3)
-    for i in range(8):
-        for j in range(5):
-            dataBanks[dataBankIndex][i][j] = dataBank[i][
-                j]  # copies data from the data bank, which was gathered during the algorithm, into data bank specifically made for that algorithm
-            dataBank[i][j] = 0
-    for q in finishedProcesses:
-        avgTat += dataBanks[dataBankIndex][q - 1][4]  # Turnaround Time
-        avgTr += dataBanks[dataBankIndex][q - 1][2]  # Response Time
-        avgTwt += dataBanks[dataBankIndex][q - 1][3]  # Waiting Time
-    avgTimes[dataBankIndex][2] = avgTat / len(finishedProcesses)  # Turnaround Time
-    avgTimes[dataBankIndex][0] = avgTr / len(finishedProcesses)  # Response Time
-    avgTimes[dataBankIndex][1] = avgTwt / len(finishedProcesses)  # Waiting Time
-
-
 def sortReadyQueue(readyQueue, processList):
     if len(readyQueue) > 1:
         n = len(readyQueue)
@@ -139,13 +97,10 @@ def sortReadyQueue(readyQueue, processList):
         pass
 
 
-
-
 def saveData(dataBankIndex, wastedTime, executionTime, finishedProcesses):
     avgTat = 0
     avgTr = 0
     avgTwt = 0
-    cpuUtilTemp = 0
 
     finishedProcesses.sort()
 
@@ -154,11 +109,9 @@ def saveData(dataBankIndex, wastedTime, executionTime, finishedProcesses):
     cpuUtilization[dataBankIndex] = round(cpuUtilTemp, 3)
 
     for q in finishedProcesses:
-
         avgTr += dataBanks[dataBankIndex][q - 1][2]  # Response Time
         avgTwt += dataBanks[dataBankIndex][q - 1][3]  # Waiting Time
         avgTat += dataBanks[dataBankIndex][q - 1][4]  # Turnaround Time
-
 
     avgTimes[dataBankIndex][0] = avgTr / len(finishedProcesses)  # Response Time
     avgTimes[dataBankIndex][1] = avgTwt / len(finishedProcesses)  # Waiting Time
